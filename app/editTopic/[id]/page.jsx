0e579e607f50bec7d2ec2c644a236b5e31b27458
fvasquezl <vasquez.limon@gmail.com>
2023-08-02 22:40:35 -0700

    create,delete and edit complete

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,24 +1,28 @@
 
 import EditTopicForm from "@/components/EditTopicForm"
 
-const EditTopic = ({ params }) => {
-    const { id } = params
-
-    const getTopic = async (id) => {
+const getTopicById = async (id) => {
+    try {
         const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
-            method: "GET"
+            cache: "no-store"
         })
 
-
-        if (res.ok) {
-            router.refresh()
+        if (!res.ok) {
+            throw new Error('Failed to fetch topic')
         }
-
+        return res.json()
+    } catch (error) {
+        console.log(error)
     }
+}
 
-    console.log(id)
+const EditTopic = async ({ params }) => {
+    const { id } = params
+    const { topic } = await getTopicById(id)
+    const { title, description } = topic
+
     return (
-        <EditTopicForm />
+        <EditTopicForm id={id} title={title} description={description} />
     )
 }
 
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,23 +1,61 @@
-import React from 'react'
+"use client"
 
-const EditTopicForm = () => {
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+const EditTopicForm = ({ id, title, description }) => {
+
+    const [newTitle, setNewTitle] = useState(title)
+    const [newDescription, setNewDescription] = useState(description)
+
+    const router = useRouter()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({ newTitle, newDescription })
+            })
+
+            if (!res.ok) {
+                throw new Error("Failed to update topic")
+            }
+
+            router.refresh()
+            router.push("/")
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return (
-        <form className='flex flex-col gap-3'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
             <input
+                onChange={(e) => setNewTitle(e.target.value)}
+                value={newTitle}
                 className='border border-slate-500 px-8 py-2'
                 type="text"
                 placeholder='Topic title' />
 
             <input
+                onChange={(e) => setNewDescription(e.target.value)}
+                value={newDescription}
                 className='border border-slate-500 px-8 py-2'
                 type="text"
                 placeholder='Topic description' />
 
             <button
+                type="submit"
                 className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
                 Update Topic
             </button>
-        </form>
+        </form> 
     )
 }
 
